d46dd0e9ce2bab895eeba08baf6f4a8fa1ac1cd6
Huikyeong <dksrud0907@kaist.ac.kr>
2021-11-03 15:56:28 +0900

    Refactor Chat.js to communicate using socket

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { ChatContainer, Expand, ChatContent, Input, Enter } from './styled';
 import { enter_btn, expand_btn } from '@/images/etc';
 
-const Chat = () => (
-  <ChatContainer>
-    <Expand src={expand_btn} />
-    <ChatContent>
-      <Input placeholder="send a message..." />
-      <Enter src={enter_btn} />
-    </ChatContent>
-  </ChatContainer>
-);
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:80/');
+socket.emit('init', { name: 'huikyeong' });
+
+const Chat = () => {
+  // const [chatArr, setChatArr] = useState([]);
+  const [chat, setChat] = useState({ name: 'huikyeong', message: '' });
+
+  socket.on('msg-rcv', item => {
+    console.log(
+      'received message from ' + item.name + ': [ ' + item.message + ' ]'
+    );
+  });
+
+  const onClickSend = useCallback(() => {
+    console.log('onclick....');
+    const str = 'hi';
+    socket.emit('msg-snd', { name: chat.name, message: chat.message });
+  }, [chat]);
+
+  const changeMessage = useCallback(
+    e => {
+      setChat({ name: chat.name, message: e.target.value });
+    },
+    [chat]
+  );
+
+  return (
+    <ChatContainer>
+      <Expand src={expand_btn} />
+      <ChatContent>
+        <Input placeholder="send a message..." onChange={changeMessage} />
+        <Enter src={enter_btn} onClick={onClickSend} />
+      </ChatContent>
+    </ChatContainer>
+  );
+};
 
 export default Chat;
