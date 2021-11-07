01ad50fa1caecf483eb943015dc6a705904a17d3
KSB21ST <21superbaby@gmail.com>
2021-11-07 21:37:14 +0900

    getAllusers undefined

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -8,14 +8,15 @@ const initialstate = {
 export default function (state = initialstate, action) {
   switch (action.type) {
     case types.GET_ALL_USERS: //모든 유저들을 화면에 띄워주기 위해 가져온다
+      if (action.payload === null) {
+        return state;
+      }
       return { ...state, userList: action.payload };
     case types.REGISTER_USER: //처음에 join 버튼 누를 때
       return { ...state, loginUser: action.payload };
     case types.MODIFY_USER: //처음에 join 버튼 누를 때
       return { ...state, loginUser: action.payload };
     case types.CHOOSE_EMOGEE: //emogee 버튼
-      console.log('choose emogee');
-      console.log(state['loginUser']);
       return {
         ...state,
         loginUser: { ...state['loginUser'], emogee: action.payload },
