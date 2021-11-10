2bef91ba954d97ff83bd4b8e85999056737e0100
KSB21ST <21superbaby@gmail.com>
2021-11-10 23:51:42 +0900

    minigame two synch

--- src/pages/MiniGame2/MiniGame2.js
+++ src/pages/MiniGame2/MiniGame2.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Header from '@/components/Header';
 import Emoticon from '@/components/Emoticon';
 import Chat from '@/components/Chat';
@@ -8,6 +8,7 @@ import {
   LowerContainer,
   C1,
 } from './styled';
+import MinigameTimer from '@/components/MinigameTimer';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllUser, setCheerScore } from '@/actions/actions';
 import { useHistory } from 'react-router';
@@ -26,6 +27,36 @@ const MiniGame2 = () => {
   const history = useHistory();
   const [_position, setPosition] = useState(0);
   const [_flag, setFlag] = useState(false);
+  const [minutes, setMinutes] = useState(2);
+  const [seconds, setSeconds] = useState(0);
+
+  socket.on('minigame2-start-rcv', item => {
+    console.log('got message');
+    setFlag(true);
+  });
+
+  useRef(() => {
+    // socket.on('minigame2-start-rcv', item => {
+    //   setFlag(true);
+    // });
+    if (_flag) {
+      const countdown = setInterval(() => {
+        if (parseInt(seconds) > 0) {
+          setSeconds(parseInt(seconds) - 1);
+          setPosition(seconds);
+        }
+        if (parseInt(seconds) === 0) {
+          if (parseInt(minutes) === 0) {
+            clearInterval(countdown);
+          } else {
+            setMinutes(parseInt(minutes) - 1);
+            setSeconds(59);
+          }
+        }
+      }, 1000);
+      return () => clearInterval(countdown);
+    }
+  }, [minutes, seconds]);
 
   dispatch(getAllUser());
 
@@ -33,31 +64,39 @@ const MiniGame2 = () => {
     setFlag(true);
   });
 
-  setInterval(() => {
-    if (_flag) {
-      setPosition(_position + 5);
-      console.log(_position);
-    }
-  }, 1000);
+  const handleStartGame = () => {
+    setFlag(true);
+  };
 
-  useEffect(() => {
-    socket.on('kickout-rcv', item => {
-      history.push('/');
-    });
-    socket.on('minigame-cheer-rcv', item => {
-      if (item.cheer === '1') {
-        dispatch(setCheerScore(item.a_score, item.b_score));
-      }
-    });
-  }, [_position]);
+  // setInterval(() => {
+  //   if (_flag) {
+  //     setPosition(_position + 5);
+  //     console.log(_position);
+  //   }
+  // }, 1000);
+
+  // useEffect(() => {
+  //   socket.on('kickout-rcv', item => {
+  //     history.push('/');
+  //   });
+  //   socket.on('minigame-cheer-rcv', item => {
+  //     if (item.cheer === '1') {
+  //       dispatch(setCheerScore(item.a_score, item.b_score));
+  //     }
+  //   });
+  // }, [_position]);
 
   return (
     <MainContainer tableIndex="0">
       <Header />
-      <UpperContainer />
+      {/* <h2>
+        {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+      </h2> */}
+      {/* <UpperContainer /> */}
       <LowerContainer>
-        <MinigameCharacter game={2} />
-        <C1 position={_position} />
+        <MinigameTimer />
+        {/* <MinigameCharacter game={2} />
+        <C1 position={_position} /> */}
       </LowerContainer>
       {/* <LowerBackContainer /> */}
       <CommunicationContent>
