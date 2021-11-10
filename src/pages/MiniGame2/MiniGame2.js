49233bfcfd209ce00e131f53c957b596928849af
amy279 <amy279@kaist.ac.kr>
2021-11-11 00:16:19 +0900

    minigame2 timer resolved

--- src/pages/MiniGame2/MiniGame2.js
+++ src/pages/MiniGame2/MiniGame2.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback, useRef } from 'react';
 import Header from '@/components/Header';
 import Emoticon from '@/components/Emoticon';
 import Chat from '@/components/Chat';
@@ -19,51 +19,64 @@ import MinigameCharacter from '@/components/MinigameCharacter';
 
 const socket = io.connect('http://localhost:80/');
 
+const useCounter = (initialValue, ms) => {
+  const [count, setCount] = useState(initialValue);
+  const intervalRef = useRef(null);
+  const start = useCallback(() => {
+    if (intervalRef.current != null) {
+      return;
+    }
+    intervalRef.current = setInterval(() => {
+      setCount(c => c + 1);
+    }, 0.1);
+  }, []);
+  const stop = useCallback(() => {
+    if (intervalRef.current == null) {
+      return;
+    }
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+  }, []);
+  const reset = useCallback(() => {
+    setCount(0);
+    stop();
+  }, []);
+  return { count, start, stop, reset };
+};
+
 const MiniGame2 = () => {
   const a_team = useSelector(state => state.user.a_team);
   const b_team = useSelector(state => state.user.b_team);
   const dispatch = useDispatch();
   const history = useHistory();
   const [_position, setPosition] = useState(0);
-  const [minutes, setMinutes] = useState(2);
-  const [seconds, setSeconds] = useState(0);
   const [_flag, setFlag] = useState(false);
   const [cnt, setCnt] = useState(0);
 
   dispatch(getAllUser());
 
-  /*useEffect(() => {
-    const countdown = setInterval(() => {
-      if (_flag) {
-        if (seconds > 0) {
-          setSeconds(seconds - 1);
-          // setPosition(seconds);
-          setPosition(_position + 1);
-        }
-        if (seconds === 0) {
-          if (minutes === 0) {
-            clearInterval(countdown);
-          } else {
-            setMinutes(minutes - 1);
-            setSeconds(59);
-          }
-        }
-      }
-    }, 50);
-    return () => clearInterval(countdown);
-  }, [minutes, seconds]);*/
-
-  //////////////////////////////////
-  /*socket.on('minigame2-start-rcv', item => {
-    setFlag(true);
-    console.log(_flag);
-  });
+  const [currentHours, setCurrentHours] = useState(0);
+  const [currentMinutes, setCurrentMinutes] = useState(0);
+  const [currentSeconds, setCurrentSeconds] = useState(0);
+  const { count, start, stop, reset } = useCounter(0, 1000);
 
   socket.on('minigame2-start-rcv', item => {
-    setPosition(_position => _position + 5);
-    console.log(_position);
-  });*/
+    console.log('got message');
+    start();
+  });
 
+  const timer = () => {
+    const checkMinutes = Math.floor(count / 60);
+    const hours = Math.floor(count / 3600);
+    const minutes = checkMinutes % 60;
+    const seconds = count / 2;
+    setCurrentHours(hours);
+    setCurrentSeconds(seconds);
+    setPosition(seconds);
+    setCurrentMinutes(minutes);
+  };
+
+  useEffect(timer, [count]);
   useEffect(() => {
     socket.on('kickout-rcv', item => {
       history.push('/');
@@ -72,13 +85,6 @@ const MiniGame2 = () => {
       if (item.cheer === '1') {
         dispatch(setCheerScore(item.a_score, item.b_score));
       }
-    });
-    socket.on('minigame2-start-rcv', item => {
-      setCnt(cnt + 1);
-      console.log(cnt);
-      setInterval(() => {
-        setPosition(_position + 2);
-      }, 1000);
     });
   }, []);
 
