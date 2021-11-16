d82680cb9b0fd43e481163f92ad78d9a2c57d137
RyuChigon <fbclrhs10@gmail.com>
2021-11-16 17:05:39 +0900

    Fix(AdminMain): add useState

--- src/pages/AdminMain/AdminMain.js
+++ src/pages/AdminMain/AdminMain.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '@/components/Header';
 import AdminChat from '@/components/AdminChat';
 import SelectMiniGame from '@/components/SelectMiniGame';
