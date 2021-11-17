e6f168672c886cf6a2257e7c155a06d3a7e6181f
RyuChigon <fbclrhs10@gmail.com>
2021-11-18 01:58:57 +0900

    Rollback Character (no direction)

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -71,13 +71,8 @@ const Character = ({
   const [_position, setPosition] = useState([0, 0]);
   const [_cheer, setCheer] = useState(false);
   const [_emoticon, setEmo] = useState('');
-  const [_direction, setDirection] = useState(0);
   const _loginUser = useSelector(state => state.user.loginUser);
   const movedistance = 10;
-  const FRONT = 0;
-  const BACK = 1;
-  const LEFT = 2;
-  const RIGHT = 3;
   let emoTimer;
 
   const size = {
@@ -91,7 +86,6 @@ const Character = ({
       setEmo(emoticon);
       socket.on('move-rcv', item => {
         if (item.name === userName) {
-          if (item.direction != _direction) setDirection(item.direction);
           setPosition(item.movement);
         }
       });
@@ -107,10 +101,8 @@ const Character = ({
       socket.on('cheer-rcv', item => {
         if (item.name === userName) {
           if (item.cheer === '0') {
-            setDirection(FRONT);
             setCheer(false);
           } else {
-            setDirection(FRONT);
             setCheer(true);
           }
         }
@@ -138,186 +130,89 @@ const Character = ({
     });
   }, []);
 
-  const characterImage = useCallback(() => {
+  const characterImage = () => {
     switch (character) {
       case 'a':
-        if (team === 'a') {
-          if (_cheer) return chigon_cheer_h;
-          else {
-            switch (_direction) {
-              case FRONT:
-                return chigon_default_h;
-              case BACK:
-                return chigon_back_h;
-              case LEFT:
-                return chigon_left_h;
-              case RIGHT:
-                return chigon_right_h;
-            }
-          }
-        } else {
-          if (_cheer) return chigon_cheer_s;
-          else {
-            switch (_direction) {
-              case FRONT:
-                return chigon_default_s;
-              case BACK:
-                return chigon_back_s;
-              case LEFT:
-                return chigon_left_s;
-              case RIGHT:
-                return chigon_right_s;
-            }
-          }
-        }
+        return team === 'a'
+          ? _cheer
+            ? chigon_cheer_h
+            : chigon_default_h
+          : _cheer
+          ? chigon_cheer_s
+          : chigon_default_s;
       case 'b':
-        if (team === 'a') {
-          if (_cheer) return b_cheer_h;
-          else {
-            switch (_direction) {
-              case FRONT:
-                return b_default_h;
-              case BACK:
-                return b_back_h;
-              case LEFT:
-                return b_left_h;
-              case RIGHT:
-                return b_right_h;
-            }
-          }
-        } else {
-          if (_cheer) return b_cheer_s;
-          else {
-            switch (_direction) {
-              case FRONT:
-                return b_default_s;
-              case BACK:
-                return b_back_s;
-              case LEFT:
-                return b_left_s;
-              case RIGHT:
-                return b_right_s;
-            }
-          }
-        }
+        return team === 'a'
+          ? _cheer
+            ? b_cheer_h
+            : b_default_h
+          : _cheer
+          ? b_cheer_s
+          : b_default_s;
       case 'c':
-        if (team === 'a') {
-          if (_cheer) return dain_cheer_h;
-          else {
-            switch (_direction) {
-              case FRONT:
-                return dain_default_h;
-              case BACK:
-                return dain_back_h;
-              case LEFT:
-                return dain_left_h;
-              case RIGHT:
-                return dain_right_h;
-            }
-          }
-        } else {
-          if (_cheer) return dain_cheer_s;
-          else {
-            switch (_direction) {
-              case FRONT:
-                return dain_default_s;
-              case BACK:
-                return dain_back_s;
-              case LEFT:
-                return dain_left_s;
-              case RIGHT:
-                return dain_right_s;
-            }
-          }
-        }
+        return team === 'a'
+          ? _cheer
+            ? dain_cheer_h
+            : dain_default_h
+          : _cheer
+          ? dain_cheer_s
+          : dain_default_s;
       case 'd':
-        if (team === 'a') {
-          if (_cheer) return subin_cheer_h;
-          else {
-            switch (_direction) {
-              case FRONT:
-                return subin_default_h;
-              case BACK:
-                return subin_back_h;
-              case LEFT:
-                return subin_left_h;
-              case RIGHT:
-                return subin_right_h;
-            }
-          }
-        } else {
-          if (_cheer) return subin_cheer_s;
-          else {
-            switch (_direction) {
-              case FRONT:
-                return subin_default_s;
-              case BACK:
-                return subin_back_s;
-              case LEFT:
-                return subin_left_s;
-              case RIGHT:
-                return subin_right_s;
-            }
-          }
-        }
+        return team === 'a'
+          ? _cheer
+            ? subin_cheer_h
+            : subin_default_h
+          : _cheer
+          ? subin_cheer_s
+          : subin_default_s;
       default:
         return chigon_default_h;
     }
-  }, [_cheer, _direction]);
+  };
 
   const moveCharacter = useCallback(e => {
     switch (e.key) {
       case 'ArrowLeft': {
         if (_position[1] - movedistance > -80) {
-          setDirection(LEFT);
           setPosition([_position[0], _position[1] - movedistance]);
           socket.emit('move-snd', {
             name: _loginUser['userName'],
             movement: _position,
-            direction: _direction,
           });
         }
         break;
       }
       case 'ArrowRight': {
         if (_position[1] + movedistance < size.width - 10) {
-          setDirection(RIGHT);
           setPosition([_position[0], _position[1] + movedistance]);
           socket.emit('move-snd', {
             name: _loginUser['userName'],
             movement: _position,
-            direction: _direction,
           });
         }
         break;
       }
       case 'ArrowUp': {
         if (_position[0] - movedistance > -80) {
-          setDirection(BACK);
           setPosition([_position[0] - movedistance, _position[1]]);
           socket.emit('move-snd', {
             name: _loginUser['userName'],
             movement: _position,
-            direction: _direction,
           });
         }
         break;
       }
       case 'ArrowDown': {
         if (_position[0] + movedistance < size.height - 280) {
-          setDirection(FRONT);
           setPosition([_position[0] + movedistance, _position[1]]);
           socket.emit('move-snd', {
             name: _loginUser['userName'],
             movement: _position,
-            direction: _direction,
           });
         }
         break;
       }
       case ' ': {
         request('get', '/api/user/cheering', null);
-        setDirection(FRONT);
         setCheer(true);
         socket.emit('cheer-snd', {
           name: _loginUser['userName'],
