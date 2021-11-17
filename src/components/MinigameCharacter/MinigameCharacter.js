906c405ac8ac62ee62f27ee450309c89292f67de
KSB21ST <21superbaby@gmail.com>
2021-11-18 00:44:46 +0900

    return to home userdb delete

--- src/components/MinigameCharacter/MinigameCharacter.js
+++ src/components/MinigameCharacter/MinigameCharacter.js
@@ -92,7 +92,7 @@ const MinigameCharacter = ({
               style={{ width: 1000, borderBottom: 'none' }}
             >
               <ImageList
-                rowHeight={100}
+                rowHeight={160}
                 cols={col}
                 gap={1}
                 className={'imagelist'}
